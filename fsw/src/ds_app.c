012ec36c44e8f364385cfe27f3a8a6f9d8961634
Avi <thnkslprpt@gmail.com>
2022-10-21 19:45:11 +1000

    Fix #60, Apply consistent Event ID names to common events

--- fsw/src/ds_app.c
+++ fsw/src/ds_app.c
@@ -261,7 +261,7 @@ int32 DS_AppInitialize(void)
     */
     if (Result == CFE_SUCCESS)
     {
-        CFE_EVS_SendEvent(DS_INIT_EID, CFE_EVS_EventType_INFORMATION,
+        CFE_EVS_SendEvent(DS_INIT_INF_EID, CFE_EVS_EventType_INFORMATION,
                           "Application initialized, version %d.%d.%d.%d, data at %p", DS_MAJOR_VERSION,
                           DS_MINOR_VERSION, DS_REVISION, DS_MISSION_REV, (void *)&DS_AppData);
     }
